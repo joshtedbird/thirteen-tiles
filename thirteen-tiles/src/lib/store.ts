932b83710f44c375f7f439cc2e0db19f43842b10
joshtedbird <josh.ted.bird@gmail.com>
2023-12-25 17:05:17 +1100

    new letters

--- thirteen-tiles/src/lib/store.ts
+++ thirteen-tiles/src/lib/store.ts
@@ -4,21 +4,21 @@ import type { GridObject, BankObject, Coord } from "./types"
 export const tiles = writable<GridObject[]>([])
 
 export const letters = writable<BankObject[]>([
-    { id: 0, value: "a" },
-    { id: 1, value: "a" },
+    { id: 0, value: "r" },
+    { id: 1, value: "h" },
     { id: 2, value: "e" },
-    { id: 3, value: "i" },
-    { id: 4, value: "o" },
-    { id: 5, value: "f" },
-    { id: 6, value: "g" },
-    { id: 7, value: "n" },
-    { id: 8, value: "n" },
-    { id: 9, value: "w" },
-    { id: 10, value: "s" },
-    { id: 11, value: "d" },
-    { id: 12, value: "x" },
-    { id: 13, value: "y" },
-    { id: 14, value: "l" },
+    { id: 3, value: "t" },
+    { id: 4, value: "r" },
+    { id: 5, value: "e" },
+    { id: 6, value: "v" },
+    { id: 7, value: "o" },
+    { id: 8, value: "h" },
+    { id: 9, value: "i" },
+    { id: 10, value: "g" },
+    { id: 11, value: "e" },
+    { id: 12, value: "e" },
+    { id: 13, value: "a" },
+    { id: 14, value: "w" },
 ])
 
 export const spaces = writable<Coord[]>([{ x: 0, y: 0 }])
